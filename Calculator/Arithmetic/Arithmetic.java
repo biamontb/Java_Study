1fc879ef66615337ecb73eb020cd2cfa11845e2c
Beatriz Montalvan B. <beatrizbarreto0000@gmail.com>
2026-03-06 09:54:04 -0300

    Adding project files.

---
+++ Calculator/Arithmetic/Arithmetic.java
@@ -0,0 +1,21 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+    
+    // -------- Arithmetic Operators -------- //
+    // Variables
+    int x = 10;
+    int y = 2;
+    int z;
+
+    z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // x %= y;
+
+    // -------- Order of Operations (PEMDAS) -------- //
+    // double result = 3 + 4 * (7 - 5) / 2.0;
+
+    System.out.println(z);
+    }
+}
