32cde367fa20271b052e16df77cba31aa9ec3b3f
Beatriz Montalvan B. <beatrizmb0000@gmail.com>
2026-03-11 08:59:48 -0300

    Uploading Project files.

---
+++ MathClass/MathClass_1.java
@@ -0,0 +1,20 @@
+// Code Reference: Bro Code (https://youtu.be/nle8CQXYhl4?si=YtIjFpdVSuVGMjkF)
+
+public class MathClass_1 {
+    public static void main(String[] args) {
+        
+        // Prints Math values
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        // Setting value to the Variable
+        System.out.println(Math.pow(2, 3)); // result of 2³ (2 raised to the power of 3)
+        System.out.println(Math.abs(-5)); // gets the absolute (positive) value of a number
+        System.out.println(Math.sqrt(16)); // calculates the correctly rounded positive square root of a given number
+        System.out.println(Math.round(3.14)); // round a floating-point number to the nearest integer
+        System.out.println(Math.ceil(3.14)); // round a number up to the nearest whole number (integer)
+        System.out.println(Math.floor(3.14)); // returns the largest integer value less than or equal to the argument
+        System.out.println(Math.max(10, 20)); // accepts two numeric values (of the same data type) as arguments and returns the larger of the two
+        System.out.println(Math.min(10, 20)); // returns the smaller of two numbers passed to it as arguments
+    }
+}
